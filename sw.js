2a2f036eb42761d1aa77a9658fd3775364377b7d
Edemar Edemar Muller <edemar.muller@head5.com.br>
2026-07-14 13:55:23 -0300

    Implementação do PWA

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "atitudesgeo-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.json",
+  "./js/logica.js",
+  "./js/interface.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
